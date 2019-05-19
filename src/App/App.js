3a8a631fb7526f3fd2f01c34f018dd5fdde827ba
Stefan Generalao <stefan.generalao@gmail.com>
2019-05-19 20:53:18 +0200

    Refactoring

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.scss';
-import RollingMeter from './RollingMeter/RollingMeter';
-import Error from './Error/Error';
-import { GeolocationConsumer } from './Contexts/Geolocation';
+import RollingMeter from '../RollingMeter/RollingMeter';
+import Error from '../Error/Error';
+import { GeolocationConsumer } from '../Contexts/Geolocation';
 
 const App = ({ error, speed }) => (
   <div className="app">
